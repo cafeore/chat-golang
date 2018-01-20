4b22d320e7606f0602917bbd40fa2116322ab326
Masaki Hashimura <prolab@MacBook-Pro.local>
2018-01-21 01:24:52 +0900

    チャットのが動くようにはなった

--- room.go
+++ room.go
@@ -55,26 +55,6 @@ func (r *room) run() {
 	}
 }
 
-func (c *client) read() {
-	for {
-		if _, msg, err := c.socket.ReadMessage(); err == nil {
-			c.room.forward <- msg
-		} else {
-			break
-		}
-	}
-	c.socket.Close()
-}
-
-func (c *client) write() {
-	for msg := range c.send {
-		if err := c.socket.WriteMessage(websocket.TextMessage, msg); err != nil {
-			break
-		}
-	}
-	c.socket.Close()
-}
-
 const (
 	socketBufferSize  = 1024
 	messageBufferSize = 256
