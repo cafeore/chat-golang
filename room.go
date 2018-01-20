7c79fb00f2d23c41088ae97b6ba0c21921ce1ab5
Masaki Hashimura <prolab@MacBook-Pro.local>
2018-01-20 23:41:36 +0900

    write,readメソッドの追加

--- room.go
+++ room.go
@@ -1,6 +1,30 @@
 package main
 
+import (
+	"github.com/gorilla/websocket"
+)
+
 type room struct {
 	//forwardは他のクライアントに転送するためのメッセージを保持するチャネル
 	forward chan []byte
+}
+
+func (c *client) read() {
+	for {
+		if _, msg, err := c.socket.ReadMessage(); err == nil {
+			c.room.forward <- msg
+		} else {
+			break
+		}
+	}
+	c.socket.Close()
+}
+
+func (c *client) write() {
+	for msg := range c.send {
+		if err := c.socket.WriteMessage(websocket.TextMessage, msg); err != nil {
+			break
+		}
+	}
+	c.socket.Close()
 }
